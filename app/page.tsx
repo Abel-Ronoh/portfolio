b21790c6e270c57b0e65b9d7057d64b770dbec6e
Abel-Ronoh <abellronoh@gmail.com>
2024-04-18 18:11:13 +0300

    changed the layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <>
     <Nav/>
    
-    <div className=" w-full flex flex-col justify-center h-[100vh]">
+    <div className=" w-full flex flex-col justify-end h-[100vh]">
     <h1 className="playball animate__animated animate__fadeInLeft text-custom-textcolor text-2xl lg:text-8xl lg:mx-20 my-3 mx-5 ">hello,</h1>
-    <h1 className="yatra  text-custom-lightblue text-6xl lg:text-7xl lg:mx-20 mx-5 font-bold">I'm Abel-Rono<span className="text-custom-textcolor animate__animated animate__flash animate__infinite animate__slower">h</span></h1>
+    <h1 className="yatra  text-custom-lightblue text-6xl lg:text-7xl lg:mx-20 mx-5 font-bold">Im Abel-Rono<span className="text-custom-textcolor animate__animated animate__flash animate__infinite animate__slower">h</span></h1>
     <div className="animate__animated animate__delay-1s my-10 animate__fadeInUp">
     <h1 className="unical tracking-wider  text-custom-textcolor mx-5 lg:mx-20 text-xl font-black">Software Developer & Engineer For The WEB</h1>
     <p className=" tracking-wider lg:mx-20 w-[35rem]">I have 1 Year of experience developing functional full stack applications with <span className="yatra tracking-wider text-custom-textcolor2 font-bold">REACT, NODE JS, FIREBASE & MONGODB</span>  </p>
