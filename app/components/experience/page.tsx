556fd875488f61949e4005945c320e99f628cc19
Abel-Ronoh <abellronoh@gmail.com>
2024-04-20 23:06:36 +0300

    added animation on firebase inputs

--- app/components/experience/page.tsx
+++ app/components/experience/page.tsx
@@ -32,19 +32,22 @@ export default function Experience() {
         }
         fetchExperiences();
     }, []);
-
+    var i =1;
     return (
-        <div className="h-[100vh] flex flex-col items-center my-20">
+        <div className="h-[100vh] w-full flex flex-col items-center my-20">
             <h1 className="text-5xl text-custom-lightblue ">EXPERIENCES</h1>
-            <ul>
+            <ul className='w-full h-full'>
                 {experiences.map((experience) => (
-                    <li key={experience.id} className='flex max-w-1/3 h-4/5 items-center justify-between'>
-                        <img className='w-1/4 m-20' src={experience.image} alt="" />
-                        <div className='text-end mx-20'>
+                    <li key={experience.id} className='relative flex w-full h-full items-center  justify-between'>
+                       <img className='animate__animated animate__bounceIn animate__delay-2s w-1/3 m-20' src={experience.image} alt="IMAGE" /> 
+                        <div className='animate__animated animate__fadeInUp h-full flex flex-col justify-center text-end mx-20'>
                         <h1 className='text-6xl font-bold text-custom-textcolor2'>{experience.header}</h1>
                         <p>{experience.description}</p>
                         </div>
+                        <h4 className='playball tracking-widest text-lg absolute bottom-0 right-20 font-bold'>see comments</h4>
+                        <hr className='absolute bottom-2 left-20 w-4/5 bg-white'/>
                     </li>
+                    
                 ))}
             </ul>
         </div>
