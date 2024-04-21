20d25ba24a28d0dae643603a1f5ef4de12f0290b
Abel-Ronoh <abellronoh@gmail.com>
2024-04-21 16:49:38 +0300

    added alternate display on experience component

--- app/components/experience/page.tsx
+++ app/components/experience/page.tsx
@@ -4,12 +4,17 @@ import { db } from "../../../firebase-config";
 import { getDocs, collection, DocumentData, QueryDocumentSnapshot } from 'firebase/firestore';
 
 // Define an interface for the structure of your Firestore documents
+interface comments{
+    username: string;
+    comment: string;
+}
 interface Experience {
     id: string;
     image: string;
     header: string;
     description: string;
-    // Add more properties if necessary
+    comments:comments;
+    index: number;
 }
 
 async function fetchDataFromFirestore(): Promise<Experience[]> {
@@ -32,24 +37,29 @@ export default function Experience() {
         }
         fetchExperiences();
     }, []);
-    var i =1;
+    var i=0;
+    experiences.forEach(experience =>{
+        i++;
+        experience.index = i;
+    })
     return (
         <div className="h-[100vh] w-full flex flex-col items-center my-20">
-            <h1 className="text-5xl text-custom-lightblue ">EXPERIENCES</h1>
+            <h1 className="text-5xl text-custom-lightblue">EXPERIENCES</h1>
             <ul className='w-full h-full'>
                 {experiences.map((experience) => (
-                    <li key={experience.id} className='relative flex w-full h-full items-center  justify-between'>
-                       <img className='animate__animated animate__bounceIn animate__delay-2s w-1/3 m-20' src={experience.image} alt="IMAGE" /> 
-                        <div className='animate__animated animate__fadeInUp h-full flex flex-col justify-center text-end mx-20'>
-                        <h1 className='text-6xl font-bold text-custom-textcolor2'>{experience.header}</h1>
-                        <p>{experience.description}</p>
+                    <li key={experience.id} className='relative flex w-full h-full items-center justify-between'>
+                        {experience.index}
+                        <img className={`animate__animated animate__bounceIn animate__delay-2s w-1/3 m-20 ${experience.index % 2 === 0 ? 'order-2' : 'order-1'}`} src={experience.image} alt="IMAGE" />
+                        <div className={`animate__animated animate__fadeInUp h-full flex flex-col justify-center text-end mx-20 ${experience.index % 2 === 0 ? 'order-1' : 'order-2'}`}>
+                            <h1 className='text-6xl font-bold text-custom-textcolor2'>{experience.header}</h1>
+                            <p>{experience.description}</p>
                         </div>
                         <h4 className='playball tracking-widest text-lg absolute bottom-0 right-20 font-bold'>see comments</h4>
                         <hr className='absolute bottom-2 left-20 w-4/5 bg-white'/>
                     </li>
-                    
                 ))}
             </ul>
         </div>
     );
 }
+
